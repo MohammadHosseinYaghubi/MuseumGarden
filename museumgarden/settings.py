3efd7c4153e6547f0e0451386602d2ad1b0e9bd7
Mohammad Hossein Yaghoubi Bojmaeh <mhossein.yaghubi20@gmail.com>
2025-03-18 01:58:12 +0330

    Update settings.py

--- museumgarden/settings.py
+++ museumgarden/settings.py
@@ -143,6 +143,7 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', "static")
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
