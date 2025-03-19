e662f80e2e9bb871dd603800cb8427db688fa8ed
Mohammad Hossein Yaghoubi Bojmaeh <mhossein.yaghubi20@gmail.com>
2025-03-19 13:35:23 +0330

    Update settings.py

--- museumgarden/settings.py
+++ museumgarden/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s*xb8&)5la+p(r_k8ul5=&4tipy-&@0r4r2e8ey&h30!)$na-='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =TRUE
 
 ALLOWED_HOSTS = []
 
@@ -142,7 +142,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', "static")
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
